ba2548866e9f685ffc728f627484cedacec232cc
Aniruddha Mahamuni <aniruddha27@gmail.com>
2022-06-14 07:20:01 +0530

    Added insertion sort

---
+++ Sorting/InsertionSort.js
@@ -0,0 +1,17 @@
+const nums = [8,9,1,2,3,4,5,7,6];
+
+
+function insertionSort(arr){
+	const length = arr.length;
+	let currentVal = 0;
+	for (let i = 1; i < length; i++) {
+		currentVal = arr[i];
+		for (var j = i-1; j >= 0 && arr[j] > currentVal; j--) {
+			arr[j+1] = arr[j];
+		}
+		arr[j+1] = currentVal;
+	}
+	return arr;
+}
+
+console.log(insertionSort(nums))
