ca7348b5840b9faa2932f7a4b1a108345699c3f3
Aniruddha Mahamuni <aniruddha27@gmail.com>
2022-06-23 07:44:05 +0530

    Added more methods to Singlylinked list.

--- Data-Structures/SinglyLinkedList.js
+++ Data-Structures/SinglyLinkedList.js
@@ -1,4 +1,3 @@
-
 class Node{
     constructor(val){
         this.val = val;
@@ -8,9 +7,9 @@ class Node{
 
 class SinglyLinkedList{
     constructor(){
-        this.length = 0;
         this.head = null;
         this.tail = null;
+        this.length = 0;
     }
 
     push(val){
@@ -25,6 +24,10 @@ class SinglyLinkedList{
         this.length++;
         return this;
     }
+
+    // Hi -> Hello -> ok
+    //         nt
+    //                 c
 
     pop(){
         if(!this.head) return undefined;
@@ -45,21 +48,24 @@ class SinglyLinkedList{
     }
 
     shift(){
-        if(!this.head) undefined;
-        let current = this.head;
-        this.head = current.next;
+        if(!this.head) return undefined;
+        var currentHead = this.head;
+        this.head = currentHead.next;
         this.length--;
         if(this.length === 0){
             this.tail = null;
         }
-        return current;
+        return currentHead;
     }
 
+    // Hi -> Hello -> ok
+    // Hey
+    
     unshift(val){
         const newNode = new Node(val);
         if(!this.head){
             this.head = newNode;
-            this.tail = this.head; 
+            this.tail = this.head;
         } else{
             newNode.next = this.head;
             this.head = newNode;
@@ -67,19 +73,68 @@ class SinglyLinkedList{
         this.length++;
         return this;
     }
+
+    get(index){
+        if(index < 0 || index >= this.length) return null;
+        let counter = 0;
+        let current = this.head;
+        while(counter < index){
+            current = current.next;
+            counter++;
+        }
+        return current;
+    }
+
+    set(val, index){
+        let foundNode = this.get(index);
+        if(foundNode){
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
+
+    // HI -> Hello ->
+    
+    //     Oh
+
+    insert(val, index){
+        if(index < 0 || index > this.length) return false;
+        if(index === 0) return !!this.unshift(val);
+        if(index === this.length) return !!this.push(val);
+        const newNode = new Node(val);
+        let prev = this.get(index - 1);
+        let temp = prev.next;
+        prev.next = newNode;
+        newNode.next = temp;
+        this.length++;
+        return true;
+    }
+
+    remove(index){
+        if(index < 0 || index > this.length) return undefined;
+        if(index === 0) return !!this.shift(val);
+        if(index === this.length) return !!this.pop(val);
+        let prevNode = this.get(index-1);
+        let removed = prevNode.next;
+        prevNode.next = removed.next;
+        this.length--;
+        return removed;
+    }
 }
 
 list = new SinglyLinkedList();
-// list.push("HELLO");
-// list.push("YES");
-// list.push("YES YES");
-// list.shift();
-// list.shift();
-// list.shift();
-// list.pop();
-// list.pop();
-list.unshift("HI");
-list.unshift("THERE");
-console.log(list);
-// list.pop();
+list.push("HI");
+list.push("HELLO");
+list.push("OK");
 // console.log(list);
+// console.log(list.pop());
+// console.log(list.shift());
+list.unshift("HEY");
+// console.log(list.get(1));
+// console.log(list.set('<3',1));
+// console.log(list.insert('oh',0));
+console.log((list));
+console.log(list.remove(1));
+console.log((list));
+console.log(JSON.stringify(list));
