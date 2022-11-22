5ada341f7d8dc026b170c420464c5c34130fdd17
Aniruddha Mahamuni <aniruddham27@gmail.com>
2022-11-22 07:44:28 +0530

    Create print-all-permutations-of-string.js

---
+++ Coding-problems/tricks/print-all-permutations-of-string.js
@@ -0,0 +1,58 @@
+
+Given a string S. The task is to find all permutations of a given string.
+
+Example 1:
+
+Input:
+S = ABC
+Output: ABC ACB BAC BCA CAB CBA 
+
+Example 2:
+
+Input:
+S = ABSG
+Output: ABGS ABSG AGBS AGSB ASBG ASGB
+BAGS BASG BGAS BGSA BSAG BSGA GABS
+GASB GBAS GBSA GSAB GSBA SABG SAGB
+SBAG SBGA SGAB SGBA
+
+ 
+
+Your Task:
+This is a function problem. You only need to complete the function 
+permutation that takes S as parameter and returns the list of permutations
+in lexicographically increasing order. The newline is automatically added by driver code.
+
+
+
+
+Solution:
+
+ permutation(str){
+        // code here
+        let n = str.length;
+        this.permut(str, 0, n-1);
+    }
+    
+    permut(s, l, r){
+        if(l === r){
+            console.log(s);
+            return
+        }
+        
+        for(let i=l;i <r;i++){
+            s = this.swap(s, l, i);
+            this.permut(s, l+1, r);
+            s = this.swap(s, l , i)
+        }
+    }
+    
+    
+    swap(a, i, j){
+        let temp;
+        let charArray = a.split("");
+        temp = charArray[i] ;
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return (charArray).join("");
+    }
